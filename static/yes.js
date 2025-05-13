9afd14377a2a5004cc9ab46c0421309d692b699e
Bishwo Karki <bishwokarki71@gmail.com>
2025-05-13 14:19:09 +0545

    first

--- static/yes.js
+++ static/yes.js
@@ -23,48 +23,20 @@ document.addEventListener('DOMContentLoaded', function () {
     
     // Your existing functionality here...
     const febHolidays = [
-      "Dear Baby, please feel it",
-      "Please ye pyaar ka cycle hai, achee se feel karna",
-      "Dost bhi tum, pyaar bhi tum 😊",
-      "ek bhi tum aur hazar bhi tum 🤩",
-      "dukh me tum aur khushi me tum 🤨",
-      "Jindagi ke safar me kaafi ho tum ❤",
-      "Maine shiddat se chaha hai tumhe, I love you❤️",
-      "You're my everything ✔",
-      "You are my soulmate ✨💕",
-      "And of course... wait baby.....",
-      "My future wife🤭",
-      "I will always be there for you",
-      "No matter how far we are🥰",
-      "I promise I will never multi-thread on you",
-      "You're the prettiest, cutest, funniest,",
-      "sweetest girl alive. 🤞✍",
-      "In one word, you're just perfect.😍",
-      "I'm the happiest man alive🥰",
-      "I'm so grateful I met you❤️",
-      "I'll always do my best to keep you happy🥺",
-      "Main humesa tumhre sath hun 🤞✔",
-      "kabhi khud ko akela mat samjhna💓",
-      "You are the most special person in my life🥰",
-      "I don't want to lose you 😒",
-      "And trust me I dont want anyone else 🥰",
-      "There's no one better than you❤️",
-      "You're the best baby!! 💖",
-      "Or should I say, I miss you when",
-      "something really good happens 🤨",
-      "Acche lagte hai wo pal jo tumhare",
-      "sath bitaye hain virtually or offline💓",
-      "There's some hardships, 😢",
-      "but we overcame most of them 🥰",
-      "and it made me realised how",
-      "important you are in my life❤️",
-      "This complete love cycle is just for you, 😘",
-      "my unconditional love,my private variable💖",
-      "Please take care of love who really loves you❤",
-      "I'm binary in love; you're the one and only '1 in my life",
-      "Thanks to God for helping us,ki hum logo ko milaya🙏",
-      "You're the love of my life,",
-      "I Love You So much❤️"
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      
+
+
+     
+    
     ];
     
     const ulEl = document.querySelector("ul");
